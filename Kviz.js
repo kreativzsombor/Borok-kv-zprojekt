5b93ff018f4f31eb571416e937eb68df7f5e8c2d
unknown <Hegyi Gbor@NET1.LOCAL>
2025-11-27 11:37:14 +0100

    kérdése

---
+++ Kviz.js
@@ -0,0 +1,72 @@
+const KvizKerdesek = [
+    {
+        subject: "Matematika",
+        questions: "Mikor használunk koszinusztételt?",
+        answers: ["Ha a háromszög két oldala és a közbezárt szöge ismert", "Ha a háromszög három oldala ismert", "Ha a háromszög két szöge és egy oldala ismert", "Ha a háromszög egyik szöge 90 fok"],
+        correctAns: 0
+    },
+  {
+        subject: "Matematika",
+        questions: "Mennyi szögfüggvény van?",
+        answers: ["3", "10", "Bitang sok", "Rossz válasz"],
+        correctAns: 0
+    },
+    {
+        subject: "Matematika",
+        questions: "Melyek szögfüggvények?",
+        answers: [" tg, cos, sin", "tg, cos, sus", "tgc, son, cis", "tg, cis, sin"],
+        correctAns: 0
+    },
+    {
+        subject: "Történelem",
+        questions: "Mikor szentesítették az áprilisi törvényeket?",
+        answers: ["1848. április 27", "1848. junius 13 ", "1848. április 11", "1799. április 14"],
+        correctAns: 2
+    },
+    {
+        subject: "Történelem",
+        questions: "Mikor volt a forradalom?",
+        answers: ["1849", "1847", "1848", "1799"],
+        correctAns: 2
+    },
+    {
+        subject: "Történelem",
+        questions: "Hány törvény cikk van az áprilisi törvényekben?",
+        answers: ["12", "31", "100", "53"],
+        correctAns: 1
+    },
+    {
+        subject: "Magyar",
+        questions: "Melyik évben adták ki Az Ember Tragédiája című művet?",
+        answers: ["1860", "1868", "1862", "1902"],
+        correctAns: 2
+    },
+    {
+        subject: "Magyar",
+        questions: "Hány színből áll az ember tragédiája?",
+        answers: ["8", "6", "7", "15"],
+        correctAns: 3
+    },
+    {
+        subject: "Magyar",
+        questions: "Ki írta Az ember tragédiája című művet?",
+        answers: ["VZS", "Kirby", "Madách Imre", "Petőfi Sándor"],
+        correctAns: 2
+    },
+    {
+        subject: "Bonus",
+        questions: "Kik készítették ezt a quizt?",
+        answers: ["VZS és Kirby", "Madách Imre és Petőfi Sándor", "Einstein és Newton", "A quiz maga"],
+        correctAns: 0
+    },
+]
+
+
+
+
+
+
+
+
+
+
